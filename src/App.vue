121f565230d7c722a2222c384546ea0b73427126
alessandraspalla <alespalla93@icloud.com>
2023-09-13 11:12:50 +0200

    refactoring per unire la ricerca allo show dei ristoranti con relativi piatti

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <script>
-import RestaurantCard from './components/RestaurantCard.vue';
-import TestQueryRestaurants from './components/TestQueryRestaurants.vue';
-
-export default {
-  components: {
-    RestaurantCard,
-    TestQueryRestaurants
-  },
-}
 </script>
 
 <template>
   <div>
-    <TestQueryRestaurants />
-    <!-- <RestaurantCard /> -->
     <router-link v-if="$route.name !== 'home'" to="/">Torna alla lista</router-link>
     <router-view></router-view>
   </div>
