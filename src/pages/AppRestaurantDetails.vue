9c5e901255979bfff108688cef9f44106ceb6cbb
alessandraspalla <alespalla93@icloud.com>
2023-09-20 10:25:26 +0200

    aggiunto bottone per aggiunta al carrello

--- src/pages/AppRestaurantDetails.vue
+++ src/pages/AppRestaurantDetails.vue
@@ -102,7 +102,10 @@ export default {
                 <div>
                     <strong>Descrizione:</strong> {{ dish.description }}
                 </div>
-                <div class="btn btn-primary w-25" @click="addToCart(dish)">+</div>
+                <div>
+                    <strong>Prezzo:</strong> €{{ dish.price }}
+                </div>
+                <button class="btn btn-primary" @click="addToCart(dish)"> Aggiungi al carrello </button>
             </div>
         </div>
     </div>
